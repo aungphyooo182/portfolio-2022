d4ad2248a3327dfa7602f43579dccc93cbfdb56d
aungphyooo <aungphyooo@amdon.com>
2022-06-14 15:26:53 +0630

    for mobile gmail

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,9 +26,7 @@ function isInViewport() {
 function isInViewportMobile() {
   var bounding = $("#portfolio")[0].getBoundingClientRect();
   // -404.15380859375 0 414 654.84619140625
-  if (
-    bounding.top >= -404 
-  ) {
+  if (bounding.top >= -404) {
     console.log("In the viewport! :)");
     return true;
   } else {
@@ -37,10 +35,10 @@ function isInViewportMobile() {
   }
 }
 
-$(document.body).on('touchmove', onTouchMove); // for mobile
-$(window).on('scroll', onScroll); 
+$(document.body).on("touchmove", onTouchMove); // for mobile
+$(window).on("scroll", onScroll);
 
-function onScroll () {
+function onScroll() {
   if (isInViewport() && !isCounting) {
     countAnimation("counter1", 12);
     countAnimation("counter2", 2);
@@ -48,7 +46,7 @@ function onScroll () {
   }
 }
 
-function onTouchMove () {
+function onTouchMove() {
   if (isInViewportMobile() && !isCounting) {
     countAnimation("counter1", 12);
     countAnimation("counter2", 2);
@@ -69,9 +67,28 @@ function countAnimation(element, end) {
   isCounting = true;
 }
 
-
-function submitForm(event){
-  console.log(event)
+function submitForm(event) {
+  console.log(event);
 }
 
-
+function deviceType() {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  } else if (
+    /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      ua
+    )
+  ) {
+    return "mobile";
+  }
+  return "desktop";
+}
+function emailForMobile() {
+  if (deviceType() === "mobile" || deviceType() === "tablet") {
+    var url =
+      "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=aungphyooo182@email.com";
+    window.open(url, "_blank");
+  }
+  // https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=aungphyooo182@email.com
+}
